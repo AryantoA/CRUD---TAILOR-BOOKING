524f48b7a6916cdebbcfb9cba933007ac3f42c2f
Aryanto <aryanto.aryanto@elium.academy>
2017-07-01 18:41:36 +0200

    Linked CSS for Index

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var bodyParser = require("body-parser")
 
 app.set('view engine','ejs')
 app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static('public/css'))
 
 app.get('/',function(req,res){
     res.render('index')
