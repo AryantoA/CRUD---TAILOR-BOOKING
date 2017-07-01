233215f34b2171584967be3f9ced69aab49139d0
Aryanto <aryanto.aryanto@elium.academy>
2017-07-01 17:35:28 +0200

    Basic Page

---
+++ app.js
@@ -0,0 +1,18 @@
+/// CRUD for SHOP
+
+var express = require("express")
+var app = express()
+var bodyParser = require("body-parser")
+
+app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.get('/',function(req,res){
+    res.render('home')
+})
+
+app.use('/shop',shops)
+
+app.listen(3000,()=> {
+    console.log('listening on port 3000')
+})
