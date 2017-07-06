3af0d26173e261502fa03526c711b472ccf5584a
Aryanto <aryanto.aryanto@elium.academy>
2017-07-06 11:14:07 +0200

    Finished

--- controller/tailors_controller.js
+++ controller/tailors_controller.js
@@ -1,33 +1,90 @@
 const Tailor = require('../models/Tailor')
 
 module.exports = {
-    // Getting All Tailor From Database
-    viewAllTailors(req, res) {
-        // Method from Youtube
-        Tailor.find({})
-            .exec(function (err, tailors) {
+        // Getting All Tailor From Database
+        viewAllTailors(req, res) {
+            // Method from Youtube
+            Tailor.find({})
+                .exec(function (err, tailors) {
+                    if (err) {
+                        res.send('error occured')
+                    } else {
+                        console.log(tailors);
+                        res.render('TailorsList', {
+                            tailors
+                        })
+                    }
+                });
+        },
+        viewNewTailors(req, res) {
+            res.render('newTailor', {
+                Tailor
+            })
+        },
+        newTailor(req, res) {
+            Tailor.create(req.body)
+            res.redirect('/tailors')
+
+        },
+        viewATailor(req, res) {
+            var IDTailor = req.params.id
+            Tailor.findById(IDTailor, function (err, tailor) {
                 if (err) {
-                    res.send('error occured')
+                    console.log(err)
                 } else {
-                    console.log(tailors);
-                    res.render('TailorsList', {
-                        tailors
+                    res.render('ATailor', {
+                        tailor
                     })
                 }
-            });
-    },
-    viewNewTailors(req, res) {
-        res.render('newTailor',{Tailor})
-    },
-    newTailor(req, res) {
-        Tailor.create(req.body)
-        res.redirect('/tailors')    
-        
-    },
-//    viewATailor(req, res) {
-//        IDTailor = req.params.id
-//        Tailor.findByID(IDTailor)
-//    },
+            })
+        },
+        viewUpdateATailor(req, res) {
+            var IDTailor = req.params.id
+            Tailor.findById(IDTailor, function (err, tailor) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    res.render('updateTailor', {
+                        tailor
+                    })
+                }
+            })
+        },
+        updateATailor(req, res) {
+            var IDTailor = req.params.id
+            Tailor.findOneAndUpdate(IDTailor, {
+                    "name": req.body.name,
+                    "email": req.body.email
+                },
+                function (err) {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        res.redirect("/tailors")
+                    }
+                })
 
+        },
+        viewDeleteATailor(req,res){
+            var IDTailor = req.params.id
+            Tailor.findById(IDTailor, function (err, tailor) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    res.render('deleteTailor', {
+                        tailor
+                    })
+                }
+            })
+        },
+        deleteATailor(req,res){
+            var IDTailor = req.params.id
+            Tailor.findByIdAndRemove(IDTailor,function(err){
+                if (err){
+                    console.log(err)
+                }else{
+                    res.redirect('/tailors')
+                }
+            })
+        }
 }
-
--- routes/tailors.js
+++ routes/tailors.js
@@ -5,7 +5,11 @@ const TailorController = require('../controller/tailors_controller')
 router.get('/',TailorController.viewAllTailors)
 router.get('/add',TailorController.viewNewTailors)
 router.get('/:id',TailorController.viewATailor)
+router.get('/update/:id',TailorController.viewUpdateATailor)
+router.get('/delete/:id',TailorController.viewDeleteATailor)
 
 
 router.post('/add',TailorController.newTailor)
+router.post('/update/:id',TailorController.updateATailor)
+router.post('/delete/:id',TailorController.deleteATailor)
 module.exports = router
